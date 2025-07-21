e9b88ef84d41d0849385c7b067c32e8327bb9311
nahyejin <nhj9609@feelanet.com>
2025-07-21 16:13:46 +0900

    load text

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,6 +1,7 @@
-import {useState} from "react";
-import {DatePicker} from "./ui/datePicker";
+import { useState } from "react";
+import { DatePicker } from "./ui/datePicker";
 import DefaultBox from "./ui/defaultBox";
+import Loader from "./Loader";
 
 const Summary = () => {
   const today = new Date();
@@ -10,6 +11,8 @@ const Summary = () => {
       <div className="grid w-full grid-cols-3 gap-3 h-80">
         <div className="h-full col-span-2 bg-white rounded-md border-1 border-slate-300 inset-shadow-xs"></div>
         <div className="h-full col-span-1">
+          '
+          <Loader />
           <div className="w-full text-center">{date.toLocaleDateString()}</div>
           <div className="mx-auto mt-3 w-fit">
             <DatePicker date={date} setDate={setDate} />
